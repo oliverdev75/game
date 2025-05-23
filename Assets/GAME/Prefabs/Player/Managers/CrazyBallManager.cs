a7d1a36108deb20b2cd7ec3de66335fd42aa6a6d
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-05-23 23:46:46 +0200

    Crazyball levels Finished

--- Assets/GAME/Prefabs/Player/Managers/CrazyBallManager.cs
+++ Assets/GAME/Prefabs/Player/Managers/CrazyBallManager.cs
@@ -1,15 +1,16 @@
-<<<<<<< HEAD
-=======
-using UnityEditor.Experimental.GraphView;
->>>>>>> d32e95b (Merging)
 using UnityEngine;
 
 public class CrazyBallManager : MonoBehaviour
 {
-
     private CircleCollider2D circleCollider2D;
     private Vector2 moveDirection;
+
+    public Transform ballSpriteTransform;
+    
     public float ballSpeed = 1;
+    public float maxBallSpeed = 20f;
+    public float speedIncreaseRate = 0.5f; // unidades por segundo
+
     public Vector2 corner = new Vector2(12f, 6f);
 
     void Awake()
@@ -19,55 +20,58 @@ public class CrazyBallManager : MonoBehaviour
 
     void Start()
     {
-        moveDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        moveDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float radius = transform.localScale.y * 0.5f;
+
+        // Aumenta la velocidad hasta el máximo
+        ballSpeed = Mathf.Min(ballSpeed + speedIncreaseRate * Time.deltaTime, maxBallSpeed);
+
         transform.Translate(ballSpeed * Time.deltaTime * moveDirection);
-        ballSpeed = Mathf.Clamp(ballSpeed, 1, 30);
 
-        if ((transform.position.y + radius) > corner.y)
+        Vector3 pos = transform.position;
+
+        if ((pos.y + radius) > corner.y)
         {
+            pos.y = corner.y - radius;
             moveDirection.y *= -1f;
             moveDirection.x = ChangeDirectionRandomBased(moveDirection.x);
-            Debug.Log(transform.position.y + " > " + corner.y);
         }
 
-        if ((transform.position.y - radius) < -corner.y)
+        if ((pos.y - radius) < -corner.y)
         {
+            pos.y = -corner.y + radius;
             moveDirection.y *= -1f;
             moveDirection.x = ChangeDirectionRandomBased(moveDirection.x);
-            Debug.Log(transform.position.y + " > " + corner.y);
         }
 
-        if ((transform.position.x + radius) > corner.x)
+        if ((pos.x + radius) > corner.x)
         {
+            pos.x = corner.x - radius;
             moveDirection.x *= -1f;
             moveDirection.y = ChangeDirectionRandomBased(moveDirection.y);
-            Debug.Log(transform.position.x + " > " + corner.x);
         }
+
+        if ((pos.x - radius) < -corner.x)
+        {
+            pos.x = -corner.x + radius;
+            moveDirection.x *= -1f;
+            moveDirection.y = ChangeDirectionRandomBased(moveDirection.y);
+        }
+
+        float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+        ballSpriteTransform.rotation = Quaternion.Euler(0, 0, angle * ballSpeed * 2);
         
-        if ((transform.position.x - radius) < -corner.x)
-        {
-            moveDirection.x *= -1f;
-            moveDirection.y = ChangeDirectionRandomBased(moveDirection.y);
-            Debug.Log(transform.position.x + " > " + corner.x);
-        }
+        moveDirection.Normalize();
+        transform.position = pos;
     }
 
     float ChangeDirectionRandomBased(float direction)
     {
-        if ((int) Random.Range(0f, 9f) == 0f)
-        {
-            return Random.Range(-1f, 1f);
-        }
-        else
-        {
-            return direction;
-        }
+        return (Random.Range(0, 9) == 0) ? Random.Range(-1f, 1f) : direction;
     }
 
     void OnDrawGizmosSelected()
@@ -76,5 +80,4 @@ public class CrazyBallManager : MonoBehaviour
         Gizmos.DrawSphere(corner, 0.5f);
         Gizmos.DrawSphere(-corner, 0.5f);
     }
-
 }
--- Assets/GAME/Scripts/GameLevelsControllers/Levels/CrazyBall_LevelController.cs
+++ Assets/GAME/Scripts/GameLevelsControllers/Levels/CrazyBall_LevelController.cs
@@ -1,18 +1,23 @@
+using System;
 using UnityEngine;
 
 public class CrazyBall_LevelController : MonoBehaviour, LevelControllerInterface
 {
-    public Transform crazyBallTransform;
-    
+    public CrazyBallManager crazyBall;
+
+    private void Awake()
+    {
+        crazyBall.enabled = false;
+    }
+
     public void StartLevel()
     {
-        // Se ejecuta cuando la cuenta regresiva se acaba
+        crazyBall.enabled = true;
     }
     
     public void FinishLevel()
     {
-        // Se ejecuta cuando hay 1 player vivo (ultimo en pie)
-
+        crazyBall.enabled = false;
     }
 
     public void PauseLevel()
